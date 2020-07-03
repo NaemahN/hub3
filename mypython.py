c2d9debe74830e564586592e3d6970959874082e
NaemahN <naemahnkaariba@gmail.com>
2020-07-03 11:20:15 +0000

    day 10 push

---
+++ mypython.py
@@ -0,0 +1,33 @@
+
+
+myname = "naemah nkaariba"
+mynumber = 100
+mybool = True
+
+
+
+def full_name(first, last):
+    return first + " " + last
+
+
+full_name("naemah", "nkaariba")
+
+# comments in python
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a variable assigned to a value without var or let
+# no semi-colon at the end of the code
